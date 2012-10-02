a2d250b75e0d9e3312045dc9f7766a96b699cabd
Oscar <oscar_villarreal-FIR@mckinsey.com>
2012-10-02 15:10:08 -0400

    first commit

---
+++ HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Oscar Villarreal on 10/2/12.
+//  Copyright (c) 2012 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController <UITextFieldDelegate>
+
+@property(copy, nonatomic) NSString *userName;
+
+@end
